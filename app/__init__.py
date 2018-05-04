449706438a14cc492250b9491d88766e59106f17
Juha Perälä <juha.perala@gmail.com>
2018-05-04 21:08:00 +0300

    added config loading

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from config import Config
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
 
     from app.api import api
     app.register_blueprint(api)
